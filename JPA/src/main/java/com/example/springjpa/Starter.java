57cfd0b92e0ef39636cabec8f5056cacb76ae6e3
Wojtek <creativs12@gmail.com>
2021-09-06 14:21:13 +0200

    Added commandlinerunner

---
+++ JPA/src/main/java/com/example/springjpa/Starter.java
@@ -0,0 +1,10 @@
+package com.example.springjpa;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Starter implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
