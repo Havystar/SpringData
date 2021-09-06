055e97ff3c738de4bae7a17f418b10196ebe3b9e
havystar <kamil.kowalczyk.dev@gmail.com>
2021-09-06 14:28:22 +0200

    Autowired all repositories

--- JPA/src/main/java/com/example/springjpa/Starter.java
+++ JPA/src/main/java/com/example/springjpa/Starter.java
@@ -1,8 +1,43 @@
 package com.example.springjpa;
 
+import com.example.springjpa.entity.Customer;
+import com.example.springjpa.entity.FilmCategory;
+import com.example.springjpa.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 public class Starter implements CommandLineRunner {
+    @Autowired
+    ActorRepository actorRepository;
+    @Autowired
+    AddressRepository addressRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    CityRepository cityRepository;
+    @Autowired
+    CountryRepository countryRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    FilmActorRepository filmActorRepository;
+    @Autowired
+    FilmCategoryRepository filmCategoryRepository;
+    @Autowired
+    FilmRepository filmRepository;
+    @Autowired
+    InventoryRepository inventoryRepository;
+    @Autowired
+    LanguageRepository languageRepository;
+    @Autowired
+    PaymentRepository paymentRepository;
+    @Autowired
+    RentalRepository repository;
+    @Autowired
+    StaffRepository staffRepository;
+    @Autowired
+    StoreRepository storeRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
