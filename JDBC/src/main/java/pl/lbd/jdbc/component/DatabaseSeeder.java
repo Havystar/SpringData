e43fe31405526d14dd7a665c20bcc18f20c4dd1a
Wojtek <creativs12@gmail.com>
2021-09-08 10:30:20 +0200

    Fixed inserts

--- JDBC/src/main/java/pl/lbd/jdbc/component/DatabaseSeeder.java
+++ JDBC/src/main/java/pl/lbd/jdbc/component/DatabaseSeeder.java
@@ -1,6 +1,7 @@
 package pl.lbd.jdbc.component;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,16 +13,16 @@ public class DatabaseSeeder {
     private JdbcTemplate jdbcTemplate;
 
     public void insertData() {
-        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (1,Jerzy,ASDASDAS, null)");
-        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (2,Andrzej,Kabala, null)");
-        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (3,Piotrek,ESESESE, null)");
+        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (1,'Jerzy','ASDASDAS', null)");
+        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (2,'Andrzej','Kabala', null)");
+        jdbcTemplate.execute("INSERT INTO actor( actor_id, first_name, last_name, last_update) VALUES (3,'Piotrek','ESESESE', null)");
 
-        jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (1,Zeromskiego,test,dystryk9,null,12341234,444-4,1);");
-        jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (2,Bytomska,test,dystryk1,null,12341234,444-4,1);");
-        jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (3,Katowicka,test,dystryk2,null,12341234,444-4,1\n);");
+        //jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (1,'Zeromskiego','test','dystryk9',null,'12341234','444-4',1)");
+       // jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (2,'Bytomska','test','dystryk1',null,'12341234','444-4',1)");
+       // jdbcTemplate.execute("INSERT INTO address( address_id, address, address2, district, last_update, phone, postal_code, city_id) VALUES (3,'Katowicka','test','dystryk2',null,'12341234','444-4',1)");
     }
 
-    @EventListener({ContextStartedEvent.class})
+    @EventListener({ContextRefreshedEvent.class})
     void seedData() {
         insertData();
     }
