da385067c6526d8aa5e765516c3bbf7bb531764a
havystar <kamil.kowalczyk.dev@gmail.com>
2021-09-08 12:42:29 +0200

    Fix Film, Inventory and add task 2

--- JDBC/src/main/java/pl/lbd/jdbc/Starter.java
+++ JDBC/src/main/java/pl/lbd/jdbc/Starter.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.lbd.jdbc.entity.Category;
+import pl.lbd.jdbc.entity.Film;
 import pl.lbd.jdbc.repository.ActorRepository;
 import pl.lbd.jdbc.repository.CategoryRepository;
+import pl.lbd.jdbc.repository.FilmRepository;
 
 import java.util.List;
 
@@ -15,12 +17,15 @@ public class Starter implements CommandLineRunner {
     ActorRepository actorRepository;
     @Autowired
     CategoryRepository categoryRepository;
-
+    @Autowired
+    FilmRepository filmRepository;
     @Override
     public void run(String... args) throws Exception {
         //outputs for tasks
         //Task 1
         //List<Category> task1 = categoryRepository.listAllSorted();
+        //Task 2
+        //List<Film> task2 = filmRepository.findAllByLanguage("Polski");
         //Task 4
         actorRepository.getAllActorsInFilm("Kiepscy").forEach(System.out::println);
 
--- JDBC/src/main/java/pl/lbd/jdbc/repository/FilmRepository.java
+++ JDBC/src/main/java/pl/lbd/jdbc/repository/FilmRepository.java
@@ -1,7 +1,21 @@
 package pl.lbd.jdbc.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.lbd.jdbc.entity.Film;
+import java.util.List;
 
-public interface FilmRepository extends Repository<FilmRepository,Long> {
+@Repository
+public interface FilmRepository extends PagingAndSortingRepository<Film,Long> {
+  //  @Query("select * from Film film join film.filmCategory filmCategory join filmCategory.category category WHERE category.name = ?1")
+   // List<Film> findAllByCategory(String searched, Pageable paging);
+
+    @Query("select * from film f join language l ON f.language_id=l.language_id WHERE l.name =:language")
+    List<Film> findAllByLanguage(@Param("language") String language);
+   // @Query("SELECT film from Film film join film.filmActors filmActors JOIN filmActors.actor actor where actor.firstName=?1 and actor.lastName=?2")
+    //List<Actor> findAllByActorNameAndLastName(String firstName,String lastName);
 }
